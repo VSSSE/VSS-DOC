30e0de646a6b3bff17365fb2e3103b7df6c8d1b2
schmiefri <schmiefri@gmail.com>
2017-10-30 15:42:56 +0100

    it aint pretty but it works

--- mockups/scripts/main.js
+++ mockups/scripts/main.js
@@ -1,15 +1,40 @@
+var BUTTON = '[data-mode="changeButton"]';
+var CHANGES = '[data-mode="display"]';
+var ITEMS = document.querySelectorAll(CHANGES);
+var darkMode = false
+
 function toLightMode() {
     var elements = document.getElementsByClassName("dark");
     while (elements) {
         elements[0].className = elements[0].className.replace(/dark/g, "light");
-        console.log(elements);
     }
+    darkMode = false;
+    BUTTON.textContent = "Change to dark mode";
 }
 
 function toDarkMode() {
     var elements = document.getElementsByClassName("light");
     while (elements) {
         elements[0].className = elements[0].className.replace(/light/g, "dark");
-        console.log(elements);
     }
+    darkMode = true;
+    BUTTON.textContent = "Change to light mode";
+}
+
+function changeMode() {
+    console.log("changing mode...");
+    if (darkMode = true) {
+        toLightMode();
+        darkMode = false;
+    }
+    else {
+        toDarkMode();
+        darkMode = true
+    }
+}
+
+function main() {
+    var btn = document.querySelector(BUTTON);
+    console.log(btn);
+    console.log(ITEMS);
 }
