633228f201437405e850348adb0978d5690023fc
schmiefri <schmiefri@gmail.com>
2017-11-02 09:26:41 +0100

    mockups work

--- mockups/scripts/main.js
+++ mockups/scripts/main.js
@@ -11,3 +11,10 @@ function toDarkMode() {
         elements[0].className = elements[0].className.replace(/light/g, "dark");
     }
 }
+
+function submitB() {
+    var start = document.querySelector(".site-start");
+    var result = document.querySelector(".site-results");
+    start.remove();
+    result.style.visibility = "visible";
+}
