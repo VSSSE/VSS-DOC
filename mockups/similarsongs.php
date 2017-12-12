13d62d4c42daddee3f2aaf90e697534c24d8dda1
c0d3d3v <live.for.program@gmail.com>
2017-12-13 00:23:58 +0100

    fakers :D

---
+++ mockups/similarsongs.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Search Results for soundtrack</title>
+    <link rel="stylesheet" href="./styles/mockup-general.css">
+</head>
+
+<body>
+    <div class="site light">
+        <header class="site-header">
+            <h1 class="light">MovieTunes</h1>
+        </header>
+        <aside class="sidebar">
+            <a class="light" href="./about">about</a>
+            <a class="light" href="https://github.com/VSSSE/VSS-DOC">GitHub</a>
+        </aside>
+
+	<?php
+	  $title = $_GET['soundtrack'];
+	  
+	?>
+        <p class="query-result light">
+            Similar songs to <?php echo $title;?>:
+        </p>
+	<?php
+	  if($title == "We Are Sex Bob-Onb"){
+	     echo <<<EOL
+	  <div class="results-list">
+	    <p id="errorMessage">No similar song found for this soundtrack</p>
+	    </div>
+EOL;
+	  }
+	  else{
+	  echo <<<EOL
+	  <div class="results-list">
+                <div class="result" id="result1">
+                    <span class="nr light" id="nr1">1</span>
+                    <span class="name light">We Are Sex Bob-Omb</span>
+                    <span class="artist light">Sex Bob-Omb</span>
+                 </div>
+                <div class="result" id="result2">
+                    <span class="nr light" id="nr2">2</span>
+                    <span class="name light">Scott Pilgrim</span>
+                    <span class="artist light">Plumtree</span>
+                 </div>
+                <div class="result" id="result3">
+                    <span class="nr light" id="nr3">3</span>
+                    <span class="name light">I Heard Ramona Sing</span>
+                    <span class="artist light">Frank Black</span>
+                 </div>
+                <div class="result" id="result4">
+                    <span class="nr light" id="nr4">4</span>
+                    <span class="name light">By Your Side</span>
+                    <span class="artist light">Beachwood Sparks</span>
+                 </div>
+                <div class="result" id="result5">
+                    <span class="nr light" id="nr5">5</span>
+                    <span class="name light">O Katrina!</span>
+                    <span class="artist light">Black Lips</span>
+                 </div>
+                <div class="result" id="result6">
+                    <span class="nr light" id="nr6">6</span>
+                    <span class="name light">I'm So Sad, So Very, Very Sad</span>
+                    <span class="artist light">Crash and the Boys</span>
+                 </div>
+                <div class="result" id="result7">
+                    <span class="nr light" id="nr7">7</span>
+                    <span class="name light">We Hate You Please Die</span>
+                    <span class="artist light">Crash and the Boys</span>
+                 </div>
+                <div class="result" id="result8">
+                    <span class="nr light" id="nr8">8</span>
+                    <span class="name light">Garbage Truck</span>
+                    <span class="artist light">Sex Bob-Omb</span>
+                 </div>
+                <div class="result" id="result9">
+                    <span class="nr light" id="nr9">9</span>
+                    <span class="name light">Teenage Dream</span>
+                    <span class="artist light">T. Rex</span>
+                 </div>
+                <div class="result" id="result10">
+                    <span class="nr light" id="nr10">10</span>
+                    <span class="name light">Sleazy Bed Track</span>
+                    <span class="artist light">The Bluetones</span>
+                 </div>
+                <div class="result light" id="result11">
+                    <span class="nr light" id="nr11">11</span>
+                    <span class="name light">It's Getting Boring by the Sea</span>
+                    <span class="artist light">Blood Red Shoes</span>
+                 </div>
+                <div class="result" id="result12">
+                    <span class="nr light" id="nr12">12</span>
+                    <span class="name light">Black Sheep</span>
+                    <span class="artist light">Metric</span>
+                 </div>
+                <div class="result" id="result13">
+                    <span class="nr light" id="nr13">13</span>
+                    <span class="name light">Threshold</span>
+                    <span class="artist light">Sex Bob-Omb</span>
+                 </div>
+                <div class="result" id="result14">
+                    <span class="nr light" id="nr14">14</span>
+                    <span class="name light">Anthems for a Seventeen Year-Old Girl</span>
+                    <span class="artist light">Broken Social Scene</span>
+                 </div>
+                <div class="result" id="result15">
+                    <span class="nr light" id="nr15">15</span>
+                    <span class="name light">Under My Thumb</span>
+                    <span class="artist light">The Rolling Stones</span>
+                 </div>
+                <div class="result" id="result16">
+                    <span class="nr light" id="nr16">16</span>
+                    <span class="name light">Ramona (acoustic version)</span>
+                    <span class="artist light">Beck Hansen</span>
+                 </div>
+                <div class="result" id="result17">
+                    <span class="nr light" id="nr17">17</span>
+                    <span class="name light">Ramona</span>
+                    <span class="artist light">Beck Hansen</span>
+                 </div>
+                <div class="result" id="result18">
+                    <span class="nr light" id="nr18">18</span>
+                    <span class="name light">Summertime</span>
+                    <span class="artist light">Sex Bob-Omb</span>
+                 </div>
+                <div class="result" id="result19">
+                    <span class="nr light" id="nr19">19</span>
+                    <span class="name light">Threshold (8 Bit)</span>
+                    <span class="artist light">Brian LeBarton</span>
+                 </div>
+          </div>
+EOL;
+	  }
+	  
+	?>
+            
+        <footer class="site-footer">
+            <button type="button" class="modeButton" data-mode="changeButton" id="tdm" onclick="toDarkMode()">Change to dark mode</button>
+            <button type="button" class="modeButton" data-mode="changeButton" id="tlm" onclick="toLightMode()">Change to light mode</button>
+        </footer>
+    </div>
+    <script src="scripts/main.js" charset="utf-8"></script>
+</body>
+
+</html>
